68bbf8fd86e11d0353a2cd1d17df59097100877f
Jorge Hurtado <jorge.hurtado@gnp.com.mx>
2019-08-05 17:20:44 -0500

    ajustes ejercicio clima

--- src/App.js
+++ src/App.js
@@ -1,10 +1,83 @@
-import React from 'react';
-import Header from './components/Header'
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Error from './components/Error';
+import axios from 'axios';
 
 function App() {
+
+  // State
+  const [pais, guardarPais] = useState('');
+  const [ciudad, guardarCiudad] = useState('');
+  const [error, guardarError] = useState(false);
+
+  // URL Data
+  const appID = 'c8a234c291b86312f28838ef57a74e2e';
+  const path = 'https://samples.openweathermap.org/data/2.5/weather/';
+  
+  useEffect(() => {
+    if(ciudad === '') return;
+    const consultarAPI = async() => {
+      const clima = await axios.get( path, {
+        mode: 'no-cors',
+        params: {
+          q : `${ciudad},${pais}`, 
+          appid : appID
+        },
+        headers: {
+          'Access-Control-Allow-Origin':'*',
+          'Access-Control-Allow-Headers':'application/json',
+        }
+      });
+      console.log('clima', clima)
+    // const url = `${path}?q=${ciudad},${pais}&appid=${appID}`;
+    // const respuesta = await fetch(url);
+    // const resultado = await respuesta.json();
+
+    // console.log('resultado', resultado)
+    }
+    consultarAPI();
+
+
+  }, [ciudad, pais]);
+
+
+
+  const datosConsulta = datos => {
+    if(datos.pais !== '' && datos.ciudad !== ''){
+      guardarPais(datos.pais);
+      guardarCiudad(datos.ciudad);
+      guardarError(false);
+    } else {
+      guardarError(true);
+      return;
+    }
+  }
+
+
+  let component;
+
+  if(error){
+    component = <Error mensaje="Ambos campos son obligatorios"/>;
+  } else {
+    component = null;
+  }
+
   return (
     <div className="App">
-     <Header titulo="Clima"/>
+     <Header titulo="Clima React App with Hooks"/>
+     <div className="contenedor-form">
+       <div className="container">
+          <div className="row">
+            <div className="col s12 m6">
+              <Formulario datosConsulta={datosConsulta}/>
+            </div>
+            <div className="col s12 m6">
+             {component}
+            </div>
+          </div>
+       </div>
+     </div>
     </div>
   );
 }
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Formulario() {
+
+function Formulario({ datosConsulta }) {
+
+    const [busqueda, guardarBusqueda] = useState({
+        pais: '',
+        ciudad: '',
+    });
+
+    const handleChange = e => {
+        console.log(e);
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+
+        console.log(busqueda);
+    }
+
+    const consultarClima = e => {
+        e.preventDefault();
+        datosConsulta(busqueda);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={consultarClima}
+        >
             <div className="input-field col s12">
                 <input
                     type="text"
@@ -10,6 +34,22 @@ function Formulario() {
                     id="ciudad"
                     onChange={handleChange}
                 />
+                <label htmlFor="ciudad">Ciudad</label>
+            </div>
+            <div className="input-field col s12">
+                <select onChange={handleChange} name="pais">
+                    <option value="">Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                </select>
+            </div>
+            <div className="input-field col s12">
+                <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accept-4" value="Buscar Clima"/>
             </div>
         </form>
     )
